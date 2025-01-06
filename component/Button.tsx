ea701b1c1ab02ec860ac850492fc5e8f72ef7854
Kishan <kishanshukla53@gmail.com>
2025-01-06 23:27:58 +0530

    setup of clone app

---
+++ component/Button.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Button() {
+  return (
+    <View>
+      <Text className=' text-emerald-600 text-3xl'>Button</Text>
+    </View>
+  )
+}
